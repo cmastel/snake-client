784317d2e310ed730f480d6f0368923eba993d16
Chris Mastel <chris.mastel@gmail.com>
2019-10-23 19:29:26 +0000

    started waiting for user input

--- play.js
+++ play.js
@@ -1,4 +1,28 @@
 const { connect } = require('./client.js');
+const stdin = process.stdin;
+
+// setup user interface
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  handleUserInput();
+  return stdin;
+};
+
+const handleUserInput = function() {
+  stdin.on('data', (key) => {
+    // if ctrl+c, thank the user and exit
+    if (key === '\u0003') {
+      console.log('Thanks for using me, ciao!')
+      process.exit();
+    }
+  })
+}
+
 
 console.log('Connecting....');
-connect()
+connect();
+setupInput();
