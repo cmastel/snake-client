b06d486bd4af8fc9be309d031bb5f50627652c7e
Chris Mastel <chris.mastel@gmail.com>
2019-10-23 16:37:52 +0000

    able to write initials to the server

--- client.js
+++ client.js
@@ -7,13 +7,18 @@ const connect = function() {
     port: 50541,
     })
 
-
   // interpret incoming data as text
   conn.setEncoding('utf8');
   
   conn.on('close', () => {
     console.log('you ded cuz you idled');
   });
+
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server');
+    conn.write('Name: CTM');
+  });
+
 
   return conn;
 };
